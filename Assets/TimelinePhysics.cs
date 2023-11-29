4bbf34d142bef6eb3182a0900e7893966e632e3e
Nathan Peach <144242721+PeachCube@users.noreply.github.com>
2023-11-29 10:49:37 -0400

    added a trigger volume and a timeline to play sounds with physics objects

---
+++ Assets/TimelinePhysics.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelinePhysics : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
